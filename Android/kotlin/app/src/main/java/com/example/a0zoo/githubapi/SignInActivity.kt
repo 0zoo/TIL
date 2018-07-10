f87f54e5917d6483ec46160818f6949e3d3e66ca
0zoo <e0zoo94@gmail.com>
2018-07-10 17:41:32 +0900

    create github api project

---
+++ Android/kotlin/app/src/main/java/com/example/a0zoo/githubapi/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a0zoo.githubapi
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
