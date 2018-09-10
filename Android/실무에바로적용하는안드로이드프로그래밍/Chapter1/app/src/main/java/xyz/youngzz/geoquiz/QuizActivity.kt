c91056a3d3b7dfdf13878b75bb3ceac285a15b94
0zoo <e0zoo94@gmail.com>
2018-09-10 23:57:58 +0900

    android study

---
+++ Android/실무에바로적용하는안드로이드프로그래밍/Chapter1/app/src/main/java/xyz/youngzz/geoquiz/QuizActivity.kt
@@ -0,0 +1,12 @@
+package xyz.youngzz.geoquiz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+    }
+}
