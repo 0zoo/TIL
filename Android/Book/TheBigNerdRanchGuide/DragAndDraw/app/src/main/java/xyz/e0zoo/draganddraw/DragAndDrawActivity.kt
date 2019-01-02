9e3f084c9d78f1ac46b8640df82eb4763508b875
0zoo <e0zoo94@gmail.com>
2019-01-02 16:09:05 +0900

    chapter 29

---
+++ Android/Book/TheBigNerdRanchGuide/DragAndDraw/app/src/main/java/xyz/e0zoo/draganddraw/DragAndDrawActivity.kt
@@ -0,0 +1,12 @@
+package xyz.e0zoo.draganddraw
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DragAndDrawActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drag_and_draw)
+    }
+}
